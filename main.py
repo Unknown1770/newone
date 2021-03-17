9f687df3735c9b015e56043d169a952abed3cce2
Vijayant Prateek <62292932+Unknown1770@users.noreply.github.com>
2021-03-17 19:56:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,47 +28,47 @@ recData = {'domain': "google",
 @app.route('/', methods =["GET", "POST"]) 
 def hello_world(): 
 	if request.method == "POST": 
-    recData = flask.request.json
-    city = recData['city']
-    email = recData['email']
-    name = recData['name']
-    mobile = recData['mobile']  
-    gender = recData['gender']
-    dob = recData['dob']
-    nationality = recData['nationality']
+    		recData = flask.request.json
+   		city = recData['city']
+    		email = recData['email']
+   		name = recData['name']
+   		mobile = recData['mobile']  
+   		gender = recData['gender']
+    		dob = recData['dob']
+   		nationality = recData['nationality']
     
-    docref = db.collection('Profile').document()
-    data1 = {
-           'city': city,
-           'email': email,
-           'name': name,
-           'mobile': mobile,
-           'gender': gender,
-           'dob': dob,
-           'nationality': nationality
-         }
-    docref.set(data1)
+   		docref = db.collection('Profile').document()
+   		data1 = {
+           	'city': city,
+           	'email': email,
+           	'name': name,
+           	'mobile': mobile,
+           	'gender': gender,
+           	'dob': dob,
+           	'nationality': nationality
+         	}
+    		docref.set(data1)
     
-    ldoc_id = docref.id
-    if domain == 'mobile' or domain == 'email':
-         type = "communication"
-    elif domain == 'facebook' or domain == 'github' or domain == 'linkedin':
-         type = "social"
+    		ldoc_id = docref.id
+    		if domain == 'mobile' or domain == 'email':
+    		     type = "communication"
+    		elif domain == 'facebook' or domain == 'github' or domain == 'linkedin':
+    		     type = "social"
         
-    data = {
-           'city': city,
-           'email': email,
-           'name': name,
-           'mobile': mobile,
-           'gender': gender,
-           'dob': dob,
-           'nationality': nationality
-         }
+   		 data = {
+         	  'city': city,
+         	  'email': email,
+          	  'name': name,
+          	  'mobile': mobile,
+         	  'gender': gender,
+         	  'dob': dob,
+         	  'nationality': nationality
+      		 }
              
-    lpvstatus = 'True'
-    resp = requests.post('https://us-central1-folk-dev-com-db.cloudfunctions.net/createLinkedProfile',json=data)
+    		lpvstatus = 'True'
+    		resp = requests.post('https://us-central1-folk-dev-com-db.cloudfunctions.net/createLinkedProfile',json=data)
      
-    return jsonify(response)
+    		return jsonify(response)
 
 
 if __name__ == '__main__':
